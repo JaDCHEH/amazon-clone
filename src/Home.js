4a8622da930083a037bb9ffe041c24c662fdb7c0
JaDCHeH <jadcheh@gmail.com>
2025-10-12 21:54:54 +0100

    saving first steps

---
+++ src/Home.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import './Home.css'
+import Product from './Product'
+
+function Home() {
+  return (
+    <div className="home">
+        <div className="home__container">
+            <img
+                src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+                alt="Home Banner"
+                className="home__image"
+            />
+            <div className="home__row">
+                <Product
+                    id="123456"
+                    title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+                    image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+                    price={29.99}
+                    rating={5}
+                />
+
+                <Product
+                    id="123458"
+                    title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+                    image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+                    price={99.99}
+                    rating={4}
+                />
+            </div>
+            <div className="home__row">
+                <Product 
+                    id="123459"
+                    title="Samsung wave 7 163.3 cm (64.5 inches) 4K Ultra HD Smart QLED TV (Black) (2020 Model)"
+                    image="https://m.media-amazon.com/images/I/81u-ERcjEJL._AC_SY300_SX300_QL70_FMwebp_.jpg"
+                    price={199.99}
+                    rating={3}
+                />
+                <Product
+                    id="123462"
+                    title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+                    image="https://images-na.ssl-images-amazon.com/images/I/61Gob-M3snL._AC_SX679_.jpg"
+                    price={98.99}
+                    rating={5}
+                />
+                <Product
+                    id="123461"
+                    title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+                    image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+                    price={598.99}
+                    rating={4}
+                />
+            </div>
+            <div className="home__row">
+                <Product
+                    id="123457"
+                    title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+                    image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+                    price={199.99}
+                    rating={5}
+                />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCi7xyJKnx6lHO54DflXe-_CwRtWUmwaes",
+  authDomain: "e-clone-ea21a.firebaseapp.com",
+  projectId: "e-clone-ea21a",
+  storageBucket: "e-clone-ea21a.firebasestorage.app",
+  messagingSenderId: "278006934721",
+  appId: "1:278006934721:web:185f517aa03fdb5150f840",
+  measurementId: "G-52X18T1R4Y"
+};
